1c250089d64059f085f70118679eeca60adb7e07
Joao Sardinha <joao@bob.home>
2018-02-22 00:48:28 +0000

    asd

--- src/main/java/app/utils/Auth.java
+++ src/main/java/app/utils/Auth.java
@@ -3,10 +3,10 @@ package app.utils;
 public class Auth {
 
     // Bot Varush
-    // public final static String BOT_TOKEN = "NDE0NDExMjQ0MDA3NTg3ODQw.DW5FOw.63WOMZJMGt6O-SFlnjyn6sb-yf4";
+    public final static String BOT_TOKEN = "NDE0NDExMjQ0MDA3NTg3ODQw.DW5FOw.63WOMZJMGt6O-SFlnjyn6sb-yf4";
 
     // test bot
-    public final static String BOT_TOKEN = "NDE1NTk0MTA4NDY0MjY3MjY1.DW4Lzg.FWx5rNb9-Co1gKwnbJ9H6kKdev0";
+    // public final static String BOT_TOKEN = "NDE1NTk0MTA4NDY0MjY3MjY1.DW4Lzg.FWx5rNb9-Co1gKwnbJ9H6kKdev0";
 
     public final static String BATTLERITE_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIyMWI2MzA0MC1mMTBjLTA"
             + "xMzUtOGU4Yy0wYTU4NjQ2MTRhMzQiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTE4MzIwODE3LCJwdWIiOiJzdHVubG9jay1zdHV"
