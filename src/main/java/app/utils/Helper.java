74ed0070728d9ca3472f048c6aa704fe7f2ad3b6
Joao Sardinha <joao@bob.home>
2018-03-03 22:46:14 +0000

    version

--- src/main/java/app/utils/Helper.java
+++ src/main/java/app/utils/Helper.java
@@ -24,7 +24,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Helper {
 
-    public final static String BOT_VERSION = "2.0.2";
+    public final static String BOT_VERSION = "2.0.3";
 
     public final static String COMMAND_TRIGGER = "!br";
     public final static String BATTLERITE_BASE_URL = "https://api.dc01.gamelockerapp.com/shards/global/";
